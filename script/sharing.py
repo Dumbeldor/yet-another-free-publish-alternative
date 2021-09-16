3d9d3826317bf57502113c59c17ce2cd64dcabf4
Mara <mara-li@outlook.fr>
2021-09-16 10:58:00 +0200

    🐛 with Admonition

--- script/sharing.py
+++ script/sharing.py
@@ -9,6 +9,9 @@ from datetime import datetime
 import frontmatter
 import yaml
 
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
+
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 if "script" in BASEDIR:
     BASEDIR = PurePath(BASEDIR).parents[0]
@@ -34,7 +37,8 @@ def retro(filepath):
     metadata = frontmatter.load(filepath)
     file = metadata.content.split("\n")
     for n in file:
-        if n != "\\":
+        n = n.replace('```', '') #remove solo code line ...
+        if n != "\\" :
             n = n.strip()
             notes.append(n)
     notes = [i for i in notes if i != ""]
@@ -60,7 +64,10 @@ def diff_file(file):
         meta_vault = frontmatter.load(vault_path)
         metadata_notes = remove_date_title(meta_notes)
         metadata_vault = remove_date_title(meta_vault)
-        if len(vault) == len(notes) or sorted(metadata_notes.keys()) == sorted(
+        if "Markdown" in file_name:
+            print(len(vault))
+            print(len(notes))
+        if len(vault) == len(notes) and sorted(metadata_notes.keys()) == sorted(
             metadata_vault.keys()
         ):
             return False #Not different
@@ -490,7 +497,6 @@ def convert_to_github():
                     clipboard(ori)
                     try:
                         import git
-
                         repo = git.Repo(Path(f"{BASEDIR}/.git"))
                         repo.git.add(".")
                         repo.git.commit("-m", f"{COMMIT}")
@@ -537,7 +543,6 @@ def convert_to_github():
                             clipboard(md)
                         try:
                             import git
-
                             repo = git.Repo(Path(f"{BASEDIR}/.git"))
                             repo.git.add(".")
                             repo.git.commit("-m", f"git commit {commit}")
